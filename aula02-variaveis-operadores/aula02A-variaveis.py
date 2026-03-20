0b05155a02a0985cdbc53fa51eb77ba4c45db10c
gabrielbarbosafurin@outlook.com <labsfiap@fiap.com.br>
2026-03-19 22:28:48 -0300

    aula 2 - variaveis e operadores

---
+++ aula02-variaveis-operadores/aula02A-variaveis.py
@@ -0,0 +1,31 @@
+print('Olá Mundo!')
+
+print(7+4)
+print("7+4")
+print('7'+'4') #concatenação de strings
+
+# comentários de 1 linha
+
+'''
+    comentarios de
+    multiplas linhas
+'''
+
+# VARIAVEIS
+
+nome = "Gabriel" # string
+idade = 18 # int
+peso = 57.4 # float
+
+print(nome, idade, peso)
+print(f"Oi, {nome}!!!")
+
+# INPUTS - SIMULAÇÃO DE FORMULÁRIOS NO CMD
+
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+nova_idade = idade + 1
+
+print(nome,idade)
+print(nova_idade)
