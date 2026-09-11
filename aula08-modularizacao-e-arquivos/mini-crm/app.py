3bf8be93232230b5b706d7d47a3c739aeb6fdcca
Gabriel Barbosa Furin <gabrielbarbosafurin@outlook.com>
2026-09-10 22:40:18 -0300

    aula 08 - modularização e arquivos - Mini CRM

---
+++ aula08-modularizacao-e-arquivos/mini-crm/app.py
@@ -0,0 +1,43 @@
+from model import model_lead
+import control
+
+def add_lead():
+    name = input("Nome: ")
+    email = input("E-mail: ")
+    status = input("Estado do fluxo de vendas: ")
+
+    # validar os dados
+    # modelar os dados, para isso, vamos usar o modulo - "model.py"
+    model_lead(name, email, status)
+
+    # com os dados modelados, preciso enviar para o JSON
+    # vou usar o control para enviar o dicionario do lead
+    control.create_lead(model_lead(name, email, status))
+
+    print("Lead adicionado!")
+
+def list_leads():
+    leads = control.read_leads()
+    print(leads)
+
+def main():
+    while True:
+        print("\nMini CRM de Leads")
+        print("[1] Adicionar Lead")
+        print("[2] Listar Leads")
+        print("[0] Sair do programa")
+
+        opt = input("\nEscolha uma opção: ")
+
+        if opt == "1":
+            add_lead()
+        elif opt == "2":
+            list_leads()
+        elif opt == "0":
+            print("Até mais...")
+            break
+        else:
+            print("Opção invalida")
+
+if __name__ == "__main__":
+    main()
---
+++ aula08-modularizacao-e-arquivos/mini-crm/model.py
@@ -0,0 +1,10 @@
+from datetime import date
+
+def model_lead(nome, email, status):
+    return {
+            "Name": nome,
+            "email": email,
+            "status": status,
+            "created": date.today().isoformat()
+            
+        }
